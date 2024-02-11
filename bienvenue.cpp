75d1d5b9c20db35d4e3b0baf46eedfa81dddaab1
sourour <ghsourour5@gmail.com>
2024-02-11 10:25:28 -0800

    Ajouter le fichier bienvenue.cpp avec des modificationsss22

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,7 +1,11 @@
 // TODO Indiquer ce que fait le programme
+#include <iostream>
 int main()
+
 {
-// TODO Afficher un message de bienvenue
+
+	// TODO Afficher un message de bienvenue
+std::cout << "Bienvenue le monde !" << std::endl;
 return 0;
 }
 
